7fb1e725c4dc636265eef03a601c01f339829511
karlaG6 <karlaisabelgt@gmail.com>
2025-11-23 18:55:12 -0500

    [FIX] default port 3001, and cors true

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.enableCors({
-    origin: 'http://localhost:3001', // donde corre Next.js
+    origin: '*', // donde corre Next.js
     methods: 'GET,POST,PUT,DELETE,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
